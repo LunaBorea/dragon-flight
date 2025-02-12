6a00ce6dd655632e515cac9d8d021c38eaf3510e
LunaBorea <129622236+LunaBorea@users.noreply.github.com>
2025-02-12 14:55:57 +0200

    Add GameScene to main, add class to game-scene.ts

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,13 @@
 import './style.css'
 import Phaser from 'phaser';
+import GameScene from './scenes/game-scene';
 
 const config: Phaser.Types.Core.GameConfig = {
     type: Phaser.AUTO,
     width: 800,
     height: 600,
     parent: 'body',
-    scene: [],
+    scene: [GameScene],
     physics: {
         default: 'arcade',
         arcade: {
---
+++ src/scenes/game-scene.ts
@@ -0,0 +1,21 @@
+class GameScene extends Phaser.Scene {
+    constructor() {
+        super('game-scene');
+    };
+
+    public preload() {
+        
+    };
+
+    public create() {
+        
+    }
+
+    public update(time: number, delta: number): void {
+        time;
+        delta;
+    };
+
+};
+
+export default GameScene;
